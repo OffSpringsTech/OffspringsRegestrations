4cf2411ba356f20eb30f83d94c94816c5a20745a
kishore3siddireddy <99656990+kishore3siddireddy@users.noreply.github.com>
2024-02-01 18:08:11 +0530

    add some Actions to  anchor tag rovtering login to registration form

--- Regestration.js
+++ Regestration.js
@@ -31,20 +31,23 @@ const passwordIconEl=document.getElementById("passwordIcon");
 const logInEl = document.getElementById("lodInId");
 
 const logInIdConEl = document.getElementById("logInId-con");
-const regeIdEl= document.getElementById("regeId")
+const regeIdEl= document.getElementById("regeId");
+const regeIdButtEl = document.getElementById("regeIdButt");
 
 
 logInEl.addEventListener('click',(c)=>{
   c.preventDefault();
-
-  if(logInIdConEl.style.display === 'none'){
     regeIdEl.style.display='none'
-    logInIdConEl.style.display = 'block';
-    
-  }else{
-    logInIdConEl.style.display = 'none';
-  }
+    logInIdConEl.style.display = 'block';   
 })
+
+regeIdButtEl.addEventListener('click',(r)=>{
+  r.preventDefault();
+  logInIdConEl.style.display='none'
+  regeIdEl.style.display = 'block';  
+
+})
+
 
 passwordIconEl.addEventListener('click',()=>{
   
