14da1fd1f2de74729c903efe6eac603a0350be23
ajayraju1 <ajayrajuadapa@gamil.com>
2024-02-03 23:51:00 +0530

    fetching user login details with post method

--- login.js
+++ login.js
@@ -1,27 +1,92 @@
+const logCon=document.getElementById("logCon");
 const loginFormEl=document.getElementById("loginForm");
 
 const loginPhoneNumberEl=document.getElementById("loginPhoneNumber");
-
 const loginPhoneNumberErrorEl=document.getElementById("loginPhoneNumberError");
+
 
 const loginPasswordEl=document.getElementById("loginPassword");
 const loginPasswordError=document.getElementById("loginPasswordError");
 
-const passwordIconEl=document.getElementById("passwordIcon");
 
 
+const url = 'https://socialapis-yrtm.onrender.com/user/login';
 
-console.log(loginPhoneNumberEl,loginPhoneNumberErrorEl)
+const loginPasswordIconEL=document.getElementById("loginPasswordIcon");
+
+
+loginPasswordIconEL.addEventListener('click',()=>{
+  
+    if(loginPasswordEl.type==='password'){
+      loginPasswordEl.type='text'
+      eyeOpen.classList.toggle("d-none")
+      eyeClose.classList.toggle("d-none")
+    }else{
+      loginPasswordEl.type="password"
+      eyeOpen.classList.toggle("d-none")
+      eyeClose.classList.toggle("d-none")
+    }
+})
 
 const validateInputs=(e,el)=>{
-  el.textContent=e.target.value===""?"*Required":''
+  el.textContent=e.target.value===""?"*Required":'';
 };
 
 
 loginPhoneNumberEl.addEventListener('blur',(e)=>{
   validateInputs(e,loginPhoneNumberErrorEl);
-})
+});
 
 loginPasswordEl.addEventListener('blur',(e)=>{
   validateInputs(e,loginPasswordError);
-})
+});
+
+  
+  const getDataFromDb = () => {
+
+// Log in api is working :)
+    const data = {
+      "phoneNumber": loginPhoneNumberEl.value,
+      "password": loginPasswordEl.value
+    };
+  
+    const options = {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(data)
+    };
+  
+    fetch(url, options).then(response => {
+      if (!response.ok) {
+        throw new Error(`HTTP error! Status: ${response.status}`);
+      }
+      return response.text();
+    })
+    .then(data => {
+      logCon.classList.add("login-response")
+      logCon.textContent=`YOUR DATA             ${data}`
+      console.log('Success:', data);
+    })
+    .catch(error => {
+      console.error('Error:', error);
+    });
+  
+  };
+  
+
+
+
+
+
+loginFormEl.addEventListener('submit',  (e) => {
+  e.preventDefault();
+  if(loginPhoneNumberEl.value!=='' && loginPasswordEl.value!==''){
+      getDataFromDb(); 
+  }
+  
+  else{
+    alert("ENTER Valid Details")
+  }
+});
