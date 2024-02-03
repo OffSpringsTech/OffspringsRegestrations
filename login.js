e5abd715a00eaed70fbb152dfcd13f9111d3efcc
kishore3siddireddy <99656990+kishore3siddireddy@users.noreply.github.com>
2024-02-03 11:17:51 +0530

    created new html file for login page and make some changes

--- login.js
+++ login.js
@@ -1,18 +1,28 @@
-const logInEl = document.getElementById("lodInId");
-const logInIdConEl = document.getElementById("logInId-con");
+const logInEl = document.getElementById("lodinId");
+const logInIdConEl = document.getElementById("loginId-con");
 const regeIdEl= document.getElementById("regeId");
 const regeIdButtEl = document.getElementById("regeIdButt");
+const loginFormEl=document.getElementById("login-form");
+const loginPasswordEl = document.getElementById("loginPassword");
+const loginPhoneNumber = document.getElementById("loginPhoneNumber");
 
 
-logInEl.addEventListener('click',(c)=>{
-  c.preventDefault();
-    regeIdEl.style.display='none'
-    logInIdConEl.style.display = 'block';   
-})
+// logInEl.addEventListener('click',(c)=>{
+//   c.preventDefault();
+//     regeIdEl.style.display='none'
+//     logInIdConEl.style.display = 'block';   
+// })
 
-regeIdButtEl.addEventListener('click',(r)=>{
-  r.preventDefault();
-  logInIdConEl.style.display='none'
-  regeIdEl.style.display = 'block';  
+// regeIdButtEl.addEventListener('click',(r)=>{
+//   r.preventDefault();
+//   logInIdConEl.style.display='none'
+//   regeIdEl.style.display = 'block';  
 
-})
+// })
+firstNameEl.addEventListener('blur',(e)=>{
+  validateInputs(e,firstNameError);
+});
+
+loginFormEl.addEventListener('click', (e)=>{
+ 
+} )
